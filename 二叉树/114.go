762b18440dea996b0d8a945322f5b9306dc4aff6
potatozhao <393926004@qq.com>
2020-01-09 00:25:57 +0800

    增加一种解法

--- 二叉树/114.go
+++ 二叉树/114.go
@@ -31,3 +31,22 @@ func flattenNode(node *TreeNode) *TreeNode{
         return flattenNode(tmpNode.Right)
     }
 }
+
+func flatten2(root *TreeNode)  {
+    if root == nil{
+        return
+    }
+    for root != nil{
+        if root.Left != nil{
+            tmpLeft := root.Left
+            for tmpLeft.Right != nil{
+                tmpLeft = tmpLeft.Right
+            }
+            tmpLeft.Right = root.Right
+            root.Right = root.Left
+            root.Left = nil
+        }
+        root = root.Right
+    }
+    return
+}
