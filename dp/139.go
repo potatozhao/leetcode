9b140153ed0c3957f27c96d24a895bb824fcf0a0
potatozhao <393926004@qq.com>
2020-01-23 16:00:25 +0800

    139要仔细研究

---
+++ dp/139.go
@@ -0,0 +1,18 @@
+// dp解法， 类似于背包。
+func wordBreak(s string, wordDict []string) bool {
+	dpList := make([]bool, len(s)+1)
+	wordMap := make(map[string]bool)
+	for i, _ := range wordDict {
+		wordMap[wordDict[i]] = true
+	}
+	dpList[0] = true
+	for i := 1; i <= len(s); i++ {
+		for j := 0; j < i; j++ {
+			if _, ok := wordMap[s[j:i]]; ok && dpList[j] {
+				dpList[i] = true
+				break
+			}
+		}
+	}
+	return dpList[len(s)]
+}
