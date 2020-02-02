bacd1618257649ee3d019dafaf86979394fb3464
potatozhao <393926004@qq.com>
2020-02-02 22:49:25 +0800

    309题第一种解法

---
+++ dp/309.go
@@ -0,0 +1,38 @@
+/*
+给定一个整数数组，其中第 i 个元素代表了第 i 天的股票价格 。​
+
+设计一个算法计算出最大利润。在满足以下约束条件下，你可以尽可能地完成更多的交易（多次买卖一支股票）:
+
+你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+卖出股票后，你无法在第二天买入股票 (即冷冻期为 1 天)。
+示例:
+
+输入: [1,2,3,0,2]
+输出: 3 
+解释: 对应的交易状态为: [买入, 卖出, 冷冻期, 买入, 卖出]
+*/
+
+package main
+
+// dp解法，
+func maxProfit(prices []int) int {
+    if len(prices) <=1{
+        return 0
+    }
+    dpList := make([]int, len(prices) + 2)
+    for j := 3; j< len(prices) + 2; j++{
+        for i := j - 1; i >=2; i--{
+                dpList[j] = max(dpList[j], dpList[i], dpList[i-2] + prices[j-2] - prices[i-2])
+        }
+    }
+    return dpList[len(prices) + 1]
+}
+
+func max(a, b, c int) int{
+    if a >= b && a >= c{
+        return a
+    }else if b >= a && b >= c{
+        return b
+    }
+    return c
+}
