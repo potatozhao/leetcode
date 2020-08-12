4cfa2db6b5286d9db6061f60c033c30d9ca9d430
potatozhao <393926004@qq.com>
2020-08-13 00:01:02 +0800

    提交一道题

---
+++ 二叉树/173.cpp
@@ -0,0 +1,78 @@
+/*
+173. 二叉搜索树迭代器
+难度
+中等
+
+226
+
+
+
+
+
+实现一个二叉搜索树迭代器。你将使用二叉搜索树的根节点初始化迭代器。
+
+调用 next() 将返回二叉搜索树中的下一个最小的数。
+
+ 
+
+示例：
+
+
+
+BSTIterator iterator = new BSTIterator(root);
+iterator.next();    // 返回 3
+iterator.next();    // 返回 7
+iterator.hasNext(); // 返回 true
+iterator.next();    // 返回 9
+iterator.hasNext(); // 返回 true
+iterator.next();    // 返回 15
+iterator.hasNext(); // 返回 true
+iterator.next();    // 返回 20
+iterator.hasNext(); // 返回 false
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class BSTIterator {
+public:
+    stack<TreeNode*> st;
+    
+    BSTIterator(TreeNode* root) {
+        set_stack(root);
+    }
+    void set_stack(TreeNode* node){
+        while(node != NULL){
+            st.push(node);
+            node = node->left;
+        }
+    }
+    
+    /** @return the next smallest number */
+    int next() {
+        TreeNode* tmp = st.top();
+        st.pop();
+        if(tmp->right){
+            set_stack(tmp->right);
+        }
+        return tmp->val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !st.empty();
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
